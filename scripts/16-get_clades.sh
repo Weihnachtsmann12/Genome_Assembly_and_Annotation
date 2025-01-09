313474e6f8ebe5e859c8647c31c66a941fd3aad1
Weihnachtsmann12 <matthias_rubin89@hotmail.com>
2025-01-09 21:10:51 +0100

    Update to script 16.

--- scripts/16-get_clades.sh
+++ scripts/16-get_clades.sh
@@ -9,25 +9,25 @@
 #SBATCH --partition=pibu_el8
 
 # for the LJA assembly
-WORKDIR=/data/users/mrubin/assembly_annotation_course
-OUTDIR=$WORKDIR/output/EDTA_annotation_LJA_3/assembly.fasta.mod.EDTA.raw/LTR
-IMAGE=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
-ASSEMBLY=$WORKDIR/lja_output/assembly.fasta
-DUSTED=$OUTDIR/assembly.fasta.mod.LTR.intact.fa
-
-cd $OUTDIR
-
-apptainer exec -C -H $WORKDIR -H ${pwd}:/work \
- --writable-tmpfs -u $IMAGE TEsorter $DUSTED -db rexdb-plant
-
-# for the flye assembly
 # WORKDIR=/data/users/mrubin/assembly_annotation_course
-# OUTDIR=$WORKDIR/output/EDTA_annotation_flye/assembly.fasta.mod.EDTA.raw/LTR
+# OUTDIR=$WORKDIR/output/EDTA_annotation_LJA_3/assembly.fasta.mod.EDTA.raw/LTR
 # IMAGE=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
-# ASSEMBLY=$WORKDIR/flye_output_2.9.5/assembly.fasta
-# DUSTED=$OUTDIR/assembly.fasta.mod.LTR.intact.fa.ori.dusted
+# ASSEMBLY=$WORKDIR/lja_output/assembly.fasta
+# DUSTED=$OUTDIR/assembly.fasta.mod.LTR.intact.fa
 
 # cd $OUTDIR
 
 # apptainer exec -C -H $WORKDIR -H ${pwd}:/work \
 #  --writable-tmpfs -u $IMAGE TEsorter $DUSTED -db rexdb-plant
+
+# for the flye assembly
+WORKDIR=/data/users/mrubin/assembly_annotation_course
+OUTDIR=$WORKDIR/output/EDTA_annotation_flye/assembly.fasta.mod.EDTA.raw/LTR
+IMAGE=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
+ASSEMBLY=$WORKDIR/flye_output_2.9.5/assembly.fasta
+DUSTED=$OUTDIR/assembly.fasta.mod.LTR.intact.fa.ori.dusted
+
+cd $OUTDIR
+
+apptainer exec -C -H $WORKDIR -H ${pwd}:/work \
+ --writable-tmpfs -u $IMAGE TEsorter $DUSTED -db rexdb-plant
