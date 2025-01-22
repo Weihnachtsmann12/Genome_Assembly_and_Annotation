524120462332d152f0c3340712a91eda81ea4342
Weihnachtsmann12 <matthias_rubin89@hotmail.com>
2025-01-22 13:11:00 +0100

    Updated get_fai script with a comment about it's use.

--- scripts/17-get_fai.sh
+++ scripts/17-get_fai.sh
@@ -8,19 +8,7 @@
 #SBATCH --error=/data/users/mrubin/assembly_annotation_course/error_get_fai_flye_%j.e
 #SBATCH --partition=pibu_el8
 
-# for the LJA assembly
-# WORKDIR=/data/users/mrubin/assembly_annotation_course
-# OUTDIR=$WORKDIR/lja_output
-# MODULE=SAMtools/1.13-GCC-10.3.0
-# ASSEMBLY=$WORKDIR/lja_output/assembly.fasta
-
-# cd $OUTDIR
-
-# module load $MODULE
-# samtools faidx $ASSEMBLY --fai-idx LJA.fai
-
-
-# for the flye assembly
+# Produces the index file used for the circle plot of the flye assembly
 WORKDIR=/data/users/mrubin/assembly_annotation_course
 OUTDIR=$WORKDIR/flye_output_2.9.5
 MODULE=SAMtools/1.13-GCC-10.3.0
