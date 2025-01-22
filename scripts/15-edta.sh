5de2046cba66410d0f964764005adafe2d84873d
Weihnachtsmann12 <matthias_rubin89@hotmail.com>
2025-01-22 13:09:09 +0100

    Updated EDTA script and get_clades script.

--- scripts/15-edta.sh
+++ scripts/15-edta.sh
@@ -4,15 +4,15 @@
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=30
 #SBATCH --job-name=edta
-#SBATCH --output=/data/users/mrubin/assembly_annotation_course/output_edta_LJA_3_%j.o
-#SBATCH --error=/data/users/mrubin/assembly_annotation_course/error_edta_LJA_3_%j.e
+#SBATCH --output=/data/users/mrubin/assembly_annotation_course/output_edta_flye_%j.o
+#SBATCH --error=/data/users/mrubin/assembly_annotation_course/error_edta_flye_%j.e
 #SBATCH --partition=pibu_el8
 
 WORKDIR=/data/users/mrubin/assembly_annotation_course
-OUTDIR=$WORKDIR/output/EDTA_annotation_LJA_3
+OUTDIR=$WORKDIR/output/EDTA_annotation_flye
 IMAGE=/data/courses/assembly-annotation-course/containers2/EDTA_v1.9.6.sif
 CDS="/data/courses/assembly-annotation-course/CDS_annotation/data/TAIR10_cds_20110103_representative_gene_model_updated"
-ASSEMBLY=$WORKDIR/lja_output/assembly.fasta
+ASSEMBLY=$WORKDIR/flye_output_2.9.5/assembly.fasta
 mkdir -p $OUTDIR
 
 cd $OUTDIR 
@@ -27,6 +27,7 @@ apptainer exec -C -H $WORKDIR -H ${pwd}:/work \
  --threads 30
 
 # $WORKDIR/flye_output_2.9.5/assembly.fasta
-# extract families and identities
+
+# extract families and identities of intact full length LTR-RTs
 # awk -F ';' '/ltr_identity=/ {print $4  $6}'  assembly.fasta.mod.LTR.intact.gff3 | grep 'Classification' | sed 's/Classification=/ /' | sed 's/ltr_identity=/ /' > families_identity.txt
 # awk -F ';' '/ltr_identity=/ {print $4  $6}' assembly.fasta.mod.LTR.intact.gff3 | grep 'Classification' | sed 's/Classification=/ /' | sed 's/ltr_identity=/ /' | uniq > families_identity_unique.txt
--- scripts/16-get_clades.sh
+++ scripts/16-get_clades.sh
@@ -8,19 +8,7 @@
 #SBATCH --error=/data/users/mrubin/assembly_annotation_course/error_get_clades_LJA_3_%j.e
 #SBATCH --partition=pibu_el8
 
-# for the LJA assembly
-# WORKDIR=/data/users/mrubin/assembly_annotation_course
-# OUTDIR=$WORKDIR/output/EDTA_annotation_LJA_3/assembly.fasta.mod.EDTA.raw/LTR
-# IMAGE=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
-# ASSEMBLY=$WORKDIR/lja_output/assembly.fasta
-# DUSTED=$OUTDIR/assembly.fasta.mod.LTR.intact.fa
-
-# cd $OUTDIR
-
-# apptainer exec -C -H $WORKDIR -H ${pwd}:/work \
-#  --writable-tmpfs -u $IMAGE TEsorter $DUSTED -db rexdb-plant
-
-# for the flye assembly
+# Produces file needed for cirlce plot of for the flye assembly
 WORKDIR=/data/users/mrubin/assembly_annotation_course
 OUTDIR=$WORKDIR/output/EDTA_annotation_flye/assembly.fasta.mod.EDTA.raw/LTR
 IMAGE=/data/courses/assembly-annotation-course/containers2/TEsorter_1.3.0.sif
